6dcf4cf3ecc8130ff015df7990a78268f5037a91
ronOsLo <92495345+ronOsLo@users.noreply.github.com>
2023-04-15 06:46:02 -0600

    Video 9

--- app/src/main/java/com/example/chatapp3/activities/ChatActivity.java
+++ app/src/main/java/com/example/chatapp3/activities/ChatActivity.java
@@ -1,11 +1,9 @@
 package com.example.chatapp3.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
 
-import com.example.chatapp3.R;
+import android.graphics.Bitmap;
+import android.os.Bundle;
 import com.example.chatapp3.adapters.ChatAdapter;
 import com.example.chatapp3.databinding.ActivityChatBinding;
 import com.example.chatapp3.models.ChatMessage;
@@ -14,15 +12,10 @@ import com.example.chatapp3.utilities.Constants;
 import com.example.chatapp3.utilities.PreferenceManager;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.Base64;
+import java.util.ArrayList;
 import java.util.List;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.widget.ImageView;
 
-import java.io.ByteArrayOutputStream;
 
 public class ChatActivity extends AppCompatActivity {
     private ActivityChatBinding binding;
@@ -40,16 +33,9 @@ public class ChatActivity extends AppCompatActivity {
         setListener();
         loadReceiverDetails();
     }
-
-   // private Bitmap getBitmapFromEncodedString(String encodedImage){
-       // byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT)
-       // return BitmapFactory.decodeByteArray(decodedString,0,decodedString.length)
-   // }
      private void loadReceiverDetails(){
         receiverUser=(User) getIntent().getSerializableExtra(Constants.KEY_USER);
         binding.textName.setText(receiverUser.name);
-
-
      }
 
      private void setListener(){
