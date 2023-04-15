9c5c3fb92354ce679cf232224902373a986d3f2a
ronOsLo <92495345+ronOsLo@users.noreply.github.com>
2023-04-15 09:36:50 -0600

    Video 9

--- app/src/main/java/com/example/chatapp3/activities/ChatActivity.java
+++ app/src/main/java/com/example/chatapp3/activities/ChatActivity.java
@@ -3,18 +3,27 @@ package com.example.chatapp3.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
 import com.example.chatapp3.adapters.ChatAdapter;
 import com.example.chatapp3.databinding.ActivityChatBinding;
 import com.example.chatapp3.models.ChatMessage;
 import com.example.chatapp3.models.User;
 import com.example.chatapp3.utilities.Constants;
 import com.example.chatapp3.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-
+import com.google.firebase.firestore.QuerySnapshot;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Locale;
 
 
 public class ChatActivity extends AppCompatActivity {
@@ -32,6 +41,72 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setListener();
         loadReceiverDetails();
+        init();
+        listedMesseges();
+    }
+    private void init(){
+        preferenceManager =new PreferenceManager(getApplicationContext());
+        chatMessages = new ArrayList<>();
+        chatAdapter= new ChatAdapter(
+                chatMessages,
+                getBitmapFromEncodedString(receiverUser.image),
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+        database=FirebaseFirestore.getInstance();
+    }
+    private  void sendMessege(){
+        HashMap<String,Object> Messege = new HashMap<>();
+        Messege.put(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID));
+        Messege.put(Constants.KEY_RECEIVER_ID, receiverUser.id);
+        Messege.put(Constants.KEY_MESSEGE, binding.inputMessage.getText().toString());
+        Messege.put(Constants.KEY_TIMESTAMP, new Date());
+        database.collection(Constants.KEY_COLLECTION_CHAT).add(Messege);
+        binding.inputMessage.setText(null);
+    }
+    private void listedMesseges(){
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, receiverUser.id)
+                .addSnapshotListener(eventListener);
+        database.collection(Constants.KEY_COLLECTION_CHAT)
+                .whereEqualTo(Constants.KEY_SENDER_ID, receiverUser.id)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+                .addSnapshotListener(eventListener);
+    }
+
+    private final EventListener<QuerySnapshot> eventListener = (value, error) ->{
+        if (error != null){
+            return ;
+        }if (value != null){
+            int count = chatMessages.size();
+            for (DocumentChange documentChange : value.getDocumentChanges()){
+                if (documentChange.getType()== DocumentChange.Type.ADDED){
+                    ChatMessage chatMessage=new ChatMessage();
+                    chatMessage.senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
+                    chatMessage.receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
+                    chatMessage.messege = documentChange.getDocument().getString(Constants.KEY_MESSEGE);
+                    chatMessage.dateTime = documentChange.getDocument().getString(Constants.KEY_TIMESTAMP);
+                    chatMessage.dateObject=documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
+                    chatMessages.add(chatMessage);
+                }
+
+            }
+            Collections.sort(chatMessages, (obj1, obj2)->obj1.dateObject.compareTo(obj2.dateObject));
+            if(count ==0){
+               chatAdapter.notifyDataSetChanged();
+            }else {
+                chatAdapter.notifyItemRangeInserted(chatMessages.size(), chatMessages.size());
+                binding.chatRecyclerView.smoothScrollToPosition(chatMessages.size()-1);
+            }
+            binding.chatRecyclerView.setVisibility(View.VISIBLE);
+        }
+        binding.progessbar.setVisibility(View.GONE);
+    };
+    private Bitmap getBitmapFromEncodedString(String encodedImage){
+        byte[] bytes= Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
     }
      private void loadReceiverDetails(){
         receiverUser=(User) getIntent().getSerializableExtra(Constants.KEY_USER);
@@ -40,5 +115,9 @@ public class ChatActivity extends AppCompatActivity {
 
      private void setListener(){
         binding.imageBack.setOnClickListener(v -> onBackPressed());
+        binding.sendMesseger.setOnClickListener(v -> sendMessege());
+     }
+     private String getReadableDateTime(Date date){
+        return new SimpleDateFormat("mm dd, yyyy - hh:mm: a", Locale.getDefault()).format(date);
      }
 }
--- app/src/main/java/com/example/chatapp3/models/User.java
+++ app/src/main/java/com/example/chatapp3/models/User.java
@@ -4,6 +4,6 @@ import java.io.Serializable;
 
 public class User implements Serializable
 {
-    public String name,image,email,token;
+    public String name,image,email,token,id;
 
 }
